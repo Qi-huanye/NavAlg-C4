0acbbc3d00466662506f506fcc0d7aa67047de28
wanandoris <1248864886@qq.com>
2026-06-03 19:27:35 +0800

    初始化

---
+++ NavAlg/NavAlg/setup.py
@@ -0,0 +1,19 @@
+from setuptools import setup
+from setuptools import setuptools
+
+
+setup(
+    name='usvlib4ros',
+    version='1.1.1.1',
+    packages=setuptools.find_packages(),
+    url='',
+    license='',
+    install_requires=[
+        # "roslibpy",
+        # "rosbags",
+    ],
+    author='qianlon',
+    author_email='',
+    description='',
+
+)
